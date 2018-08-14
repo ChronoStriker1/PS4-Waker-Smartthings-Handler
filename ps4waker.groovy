a857f4f3be96d63b2710f1d91ea67d3cde35f131
ChronoStriker1 <chronostriker1@gmail.com>
2018-08-13 20:49:55 -0400

    mostly working code

--- ps4waker.groovy
+++ ps4waker.groovy
@@ -10,74 +10,73 @@
 *   on an AS IS BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
 *   for the specific language governing permissions and limitations under the License.
 * 
-* 	PS4-Waker
+*   PS4-Waker
 * 
 *   Co-Author Jeffrey Schiller
 *   Date 2018-08-13 v1.0 Initial Release... Edited for PS4-Waker 
-* 	Author Stuart Buchanan
-* 	Date 2016-04-06 v1.1 Added User Authentication, added logging of response parsed http headers.
-* 	Date 2016-04-05 v1.0 Initial Release
+*   Author Stuart Buchanan
+*   Date 2016-04-06 v1.1 Added User Authentication, added logging of response parsed http headers.
+*   Date 2016-04-05 v1.0 Initial Release
 **/ 
 preferences {
-	input("ServerIP", "text", title: "ServerIP", description: "Enter the IP Address of the PS4-Waker Server")
-	input("Port", "text", title: "Port", description: "Enter the TCP Port of the PS4-Waker Server")
+    input("ServerIP", "text", title: "ServerIP", description: "Enter the IP Address of the PS4-Waker-REST Server")
+    input("Port", "text", title: "Port", description: "Enter the TCP Port of the PS4-Waker-REST Server")
     input("PS4IP", "text", title: "Port", description: "Enter the IP Address of the Playstation 4")
+
 }  
  
 metadata {
-	definition (name: "PS4-Waker", namespace: "ps4waker", author: "Jeffrey Schiller") {
-		capability "Switch"
+    definition (name: "PS4-Waker", namespace: "ps4waker", author: "Jeffrey Schiller") {
+        capability "Switch"
         capability "Refresh"
         capability "Polling"
 
         command "on"
-		command "off" 
-		command "start"
-		command "key"
+        command "off" 
         command "info" 
 }
 
 
-	// simulator metadata
+    // simulator metadata
 simulator {
-		// status messages
+        // status messages
 
-		// reply messages
+        // reply messages
 }
 
 tiles(scale: 2) {
     multiAttributeTile(name:"switch", type: "lighting", width: 6, height: 4){
         tileAttribute ("device.switch", key: "PRIMARY_CONTROL") {
-            attributeState "on", label:'Open', action:"switch.off", icon:"https://raw.githubusercontent.com/fuzzysb/SmartThings/master/DeviceTypes/fuzzysb/Axis%20Gate%20Opener/GateOpen.png", backgroundColor:"#ffa81e"
-            attributeState "off", label:'Closed', action:"switch.on", icon:"https://raw.githubusercontent.com/fuzzysb/SmartThings/master/DeviceTypes/fuzzysb/Axis%20Gate%20Opener/GateClosed.png", backgroundColor:"#79b821"
+            attributeState "on", label:'On', action:"switch.off", icon:"https://raw.githubusercontent.com/ChronoStriker1/PS4-Waker-Smartthings-Handler/master/ps4on.png"
+            attributeState "off", label:'Off', action:"switch.on", icon:"https://raw.githubusercontent.com/ChronoStriker1/PS4-Waker-Smartthings-Handler/master/ps4off.png"
         }
     }
-	standardTile("on", "device.switch", width: 2, height: 2, decoration: "flat") {
-			state "default", label:"On", action:"switch.on", icon:"https://raw.githubusercontent.com/ChronoStriker1/PS4-Waker-Smartthings-Handler/master/ps4on.png", backgroundColor:"#ffa81e"
-		}	
-	standardTile("off", "device.switch", width: 2, height: 2, decoration: "flat") {
-			state "default", label:"Off", action:"switch.off", icon:"https://raw.githubusercontent.com/ChronoStriker1/PS4-Waker-Smartthings-Handler/master/ps4off.png", backgroundColor:"#79b821"
-		}
-	standardTile("refresh", "device.switch", width: 2, height: 2, decoration: "flat") {
-			state "default", label:"", action:"refresh.refresh", icon:"st.secondary.refresh"
-		}			
+    standardTile("on", "device.switch", width: 2, height: 2, decoration: "flat") {
+            state "default", label:"On", action:"switch.on", icon:"https://raw.githubusercontent.com/ChronoStriker1/PS4-Waker-Smartthings-Handler/master/ps4on.png"
+        }   
+    standardTile("off", "device.switch", width: 2, height: 2, decoration: "flat") {
+            state "default", label:"Off", action:"switch.off", icon:"https://raw.githubusercontent.com/ChronoStriker1/PS4-Waker-Smartthings-Handler/master/ps4off.png"
+        }
+    standardTile("refresh", "device.switch", width: 2, height: 2, decoration: "flat") {
+            state "default", label:"", action:"refresh.refresh", icon:"st.secondary.refresh"
+        }           
         main "switch"
-		details(["switch","on","off","refresh"])
-	}
+        details(["switch","on","off","refresh"])
+    }
 }
 
 def updated() {
     log.info "PS4-Waker Updated"
     state.dni = createDNI(settings.ServerIP, settings.Port)
     state.hostAddress = "${settings.ServerIP}, ${settings.Port}"
-	refresh()
+    refresh()
 }
 
 def installed() {
     log.info "PS4-Waker Updated"
     state.dni = createDNI(settings.ServerIP, settings.Port)
     state.hostAddress = "${settings.ServerIP}, ${settings.Port}"
-	refresh()
+    refresh()
 }
 
 def parse(String message) {
@@ -102,10 +101,10 @@ def parse(String message) {
         log.error "No HTTP body found in '${message}'"
         return null
     } else {
-	def body = new String(msg.body.decodeBase64())
-	parseHttpResponse(body)
-	log.debug "body: ${body}"
-	}
+    def body = new String(msg.body.decodeBase64())
+    parseHttpResponse(body)
+    log.debug "body: ${body}"
+    }
 }
 
 private parseHttpHeaders(String headers) {
@@ -123,35 +122,35 @@ private parseHttpHeaders(String headers) {
 
 private def parseHttpResponse(String data) {
     log.debug("parseHttpResponse(${data})")
-	def splitresponse = data.split("=")
+    def splitresponse = data.split("=")
     def port = splitresponse[0]
-	def status = splitresponse[1]
-	if (status == "active"){
-		createEvent(name: "switch", value: "on", descriptionText: "$device.displayName is on", isStateChange: "true")
-	} else if (status == "inactive"){
-		createEvent(name: "switch", value: "off", descriptionText: "$device.displayName is off", isStateChange: "true")
-	}
+    def status = splitresponse[1]
+    if (status == "active"){
+        createEvent(name: "switch", value: "on", descriptionText: "$device.displayName is on", isStateChange: "true")
+    } else if (status == "inactive"){
+        createEvent(name: "switch", value: "off", descriptionText: "$device.displayName is off", isStateChange: "true")
+    }
     return status
 }
 
 def poll() {
-	log.debug "Executing poll Command"
-	refresh()
+    log.debug "Executing poll Command"
+    refresh()
 }
 
 def refresh() {
-	log.debug "Executing Refresh Command"
-	getstatus()
+    log.debug "Executing Refresh Command"
+    getstatus()
 }
 
 def on() {
-	log.debug "Executing On Command"
-	open()
+    log.debug "Executing On Command"
+    open()
 }
 
 def off() {
-	log.debug "Executing Off Command"
-	close()
+    log.debug "Executing Off Command"
+    close()
 }
 
 def open(){
@@ -159,14 +158,14 @@ log.debug "Checking DNI"
 updateDNI()
 try {
     log.debug "Executing On"
-	def openResult = getopen()
+    def openResult = postopen()
     log.debug "${openResult}"
-	sendHubCommand(openResult)
-	}
-	catch (Exception e)
-	{
-   		log.debug "Hit Exception $e"
-	}
+    sendHubCommand(openResult)
+    }
+    catch (Exception e)
+    {
+        log.debug "Hit Exception $e"
+    }
 createEvent(name: "switch", value: "on", descriptionText: "$device.displayName is on", isStateChange: "true")
 }
 
@@ -175,23 +174,23 @@ log.debug "Checking DNI"
 updateDNI()
 try {
     log.debug "Executing Off"
-	def closeResult = getclose()
+    def closeResult = postclose()
     log.debug "${closeResult}"
-	sendHubCommand(closeResult)
-	}
-	catch (Exception e)
-	{
-   		log.debug "Hit Exception $e"
-	}
+    sendHubCommand(closeResult)
+    }
+    catch (Exception e)
+    {
+        log.debug "Hit Exception $e"
+    }
 createEvent(name: "switch", value: "off", descriptionText: "$device.displayName is off", isStateChange: "true")
 }
 
-def getopen(){
-	//def userpassascii = "${settings.username}:${settings.password}"
+def postopen(){
+    //def userpassascii = "${settings.username}:${settings.password}"
     //def userpass = "Basic " + userpassascii.encodeAsBase64().toString()
     def headers = [:]
-	headers.put("HOST","${settings.ServerIP}:${settings.Port}")
-	//headers.put("Authorization","${userpass}")
+    headers.put("HOST","${settings.ServerIP}:${settings.Port}")
+    //headers.put("Authorization","${userpass}")
     def result = new physicalgraph.device.HubAction(
         method: "GET",
         path: "/ps4/${settings.PS4IP}/on",
@@ -200,12 +199,12 @@ def getopen(){
     return result
 }
 
-def getclose(){
-	//def userpassascii = "${settings.username}:${settings.password}"
+def postclose(){
+    //def userpassascii = "${settings.username}:${settings.password}"
     //def userpass = "Basic " + userpassascii.encodeAsBase64().toString()
     def headers = [:]
-	headers.put("HOST","${settings.ServerIP}:${settings.Port}")	
-	//headers.put("Authorization","${userpass}")
+    headers.put("HOST","${settings.ServerIP}:${settings.Port}") 
+    //headers.put("Authorization","${userpass}")
     def result = new physicalgraph.device.HubAction(
         method: "GET",
         path: "/ps4/${settings.PS4IP}/off",
@@ -215,11 +214,11 @@ def getclose(){
 }
 
 def getstatus(){
-	//def userpassascii = "${settings.username}:${settings.password}"
+    //def userpassascii = "${settings.username}:${settings.password}"
     //def userpass = "Basic " + userpassascii.encodeAsBase64().toString()
     def headers = [:]
-	headers.put("HOST","${settings.ServerIP}:${settings.Port}")	
-	//headers.put("Authorization","${userpass}")
+    headers.put("HOST","${settings.ServerIP}:${settings.Port}") 
+    //headers.put("Authorization","${userpass}")
     def result = new physicalgraph.device.HubAction(
         method: "GET",
         path: "/ps4/${settings.PS4IP}/info",
@@ -236,7 +235,7 @@ private String createDNI(ipaddr, port) {
     }.join()
 
     def hexPort = String.format('%04X', port.toInteger())
-	log.debug "Hex IP:Port: ${hexIp}:${hexPort}"
+    log.debug "Hex IP:Port: ${hexIp}:${hexPort}"
     return "${hexIp}:${hexPort}"
 }
 
